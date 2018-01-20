2dccc848b32abd0228400a9af0c9841d560658bc
avishekmaitra <33186168+avishekmaitra@users.noreply.github.com>
2018-01-20 13:33:53 -0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AmberApp
+//
+//  Created by Ryan Zhan on 1/13/18.
+//  Copyright © 2018 QL+. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+
+}
+
